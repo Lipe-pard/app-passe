f830cfce6679aa83a6c1b288d043c53b5c2c9bd8
Felipe-dev <felipe-dev@pop-os.localdomain>
2023-10-25 11:53:54 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" backgroundColor='#000'/>
+      <WebView
+      style={{ flex: 1}}
+      source={{ uri: 'https://passe-escolar.educacaocaraguatatuba.com.br/' }}
+    />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: '10%',
   },
 });
